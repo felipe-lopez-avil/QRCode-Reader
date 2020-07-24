061f4a6a1a25f7e93f38376bf603825f45921738
felipe-lopez-avil <felipe_lopez.avil@hotmail.com>
2020-07-24 10:13:27 -0600

    QR Code Reader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
+import './App.css'
+ 
+class App extends Component {
+  state = {
+    result: 'No result'
+  }
+ 
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '50%' }}
+        />
+        <p>{this.state.result}</p>
+        {console.log(this.state.result)}
+
+        <div className="container">
+          <p>Generar QR</p>
+          <input type="text" placeholder="Enter value"></input>
+          <button>
+            Generate
+          </button>
+          <div id="qrcode"></div>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
